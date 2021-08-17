6a9f1933041f9fb2601b655f71ff65b92f0948ca
watchirawit <68271994+watchirawit@users.noreply.github.com>
2021-08-17 21:39:50 +0700

    hop

---
+++ guestbook7.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Date
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://webadmin:SPYatb32373@node1247-rachpython.th.app.ruk-com.cloud:11001/testdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://webadmin:ZQQcoa88817@node17333-wachirawit.app.ruk-com.cloud:11107/mydb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    name = Column(String,primary_key=True)
+    comment = Column(String)
+
+
+@app.route('/')
+def index():
+    result = Comments.query.all()
+    return render_template('index7.html', result=result)
+
+
+@app.route('/sign')
+def sign():
+    return render_template('sign7.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+    signature = Comments(name=name, comment=comment)
+    db.session.add(signature)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
